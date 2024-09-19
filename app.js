72ae5840d71109f9f6d8f5e2f234636f54ae3660
Desduh <eduardofalandess@gmail.com>
2024-09-19 20:46:59 -0300

    adicao de cors para conexao com front

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 const express = require('express');
 const { Instancia } = require('./models/instancia');
 const instancia = require('./models/instancia');
-var bodyParser = require('body-parser')
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 app.post('/cadastro', async (req, res) => {
     var instancia = new Instancia();
     try {
-        resultado = await instancia.cadastro(req.body);
+        const resultado = await instancia.cadastro(req.body);
         res.send(resultado);
-    } catch {
+    } catch (error) {
         res.status(500).send("Erro durante o processo de cadastro de instancia.");
     }
-})
+});
 
 app.get('/visualizar', async (req, res) => {
     var instancia = new Instancia();
-
     try {
-        instancias = await instancia.visualizar();
+        const instancias = await instancia.visualizar();
         res.send(JSON.stringify(instancias));
-    } catch {
+    } catch (error) {
         res.status(500).send("Erro durante o processo de visualização de instancias.");
     }
-})
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
