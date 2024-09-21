0c1378951c6679da5f2380609210d78591c6f690
LucasFrancaRegistro <lucascumuru@gmail.com>
2024-09-20 22:34:33 -0300

    correcao na formatacao da data e ordem dos dados

--- models/instancia.js
+++ models/instancia.js
@@ -35,7 +35,14 @@ class Instancia{
     
     cadastro(data) {
         return new Promise(async (resolve, reject) => {
-            data.data = new Date();
+          let date =  new Date();
+          const formattedDate = `${String(date.getDate()).padStart(2, '0')}/${
+            String(date.getMonth() + 1).padStart(2, '0')}/${
+            date.getFullYear()} ${
+            String(date.getHours()).padStart(2, '0')}:${
+            String(date.getMinutes()).padStart(2, '0')}:${
+            String(date.getSeconds()).padStart(2, '0')}`;
+          data.data = formattedDate;
             try {
                 const insertManyResult = await collection.insertOne(data);
                 resolve(`instancia inserida.\n`);
@@ -50,7 +57,7 @@ class Instancia{
         return new Promise(async (resolve, reject) => {
 
             try {
-                var dados = collection.find({}).toArray();
+              var dados = collection.find({}).sort({ data: -1 }).toArray();
                 resolve(dados)
             } catch {
                 reject(`Não foi possivel encontrar instancias: ${err}\n`);
